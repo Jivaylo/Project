eff9b32af6bc31b2284bbe2615ef11936d446257
Ivaylo <ivaylo06@abv.bg>
2024-03-28 08:13:02 +0200

    Something

---
+++ Project/app/src/main/java/ladd/example/androidmvvmexample/utils/Extensions.kt
@@ -0,0 +1,11 @@
+package ladd.marshall.androidmvvmexample.utils
+
+import com.google.android.material.textfield.TextInputEditText
+
+
+fun TextInputEditText.toInt(): Int {
+    return when {
+        this.text.toString().isBlank() -> 0
+        else -> this.text.toString().toInt()
+    }
+}
---
+++ Project/app/src/main/java/ladd/example/androidmvvmexample/viewModel/viewModels/ListViewModel.kt
@@ -0,0 +1,20 @@
+package ladd.marshall.androidmvvmexample.viewModel.viewModels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import kotlinx.coroutines.Dispatchers
+import ladd.marshall.androidmvvmexample.model.models.Employee
+import ladd.marshall.androidmvvmexample.viewModel.repositories.EmployeeRepository
+
+class ListViewModel(application: Application) : AndroidViewModel(application) {
+
+
+    private val employeeRepository = EmployeeRepository.getInstance(application)
+
+
+    val employeeListLiveData: LiveData<List<Employee>> = liveData(Dispatchers.IO) {
+        emitSource(employeeRepository.getAllEmployeesLiveData())
+    }
+}
